09083ecbb59d331ff6c0a415e7bf2161ac4194be
Edward Källstedt <edward.kallstedt@gmail.com>
2025-02-12 16:03:39 +0100

    fix lobby view not updating when players join

--- frontend/src/lib/components/game/lobby-view.svelte
+++ frontend/src/lib/components/game/lobby-view.svelte
@@ -5,8 +5,7 @@
 	import { Button } from '$lib/components/ui/button';
 	import { ScrollArea } from '$lib/components/ui/scroll-area/index.js';
 
-	$: players = Array.from(gameStore.state.players.values());
-
+	const players = $derived(Array.from(gameStore.state.players.values()));
 	function handleLeaveGame() {
 		gameActions.leaveGame();
 	}
