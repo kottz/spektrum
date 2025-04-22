aa6f74b2f2051a94aaa2fcd66a2626b88d7285f4
Edward Källstedt <edward.kallstedt@gmail.com>
2025-04-22 10:16:45 +0200

    frontend: fix invalid join link page

--- frontend/src/routes/join/[...bad]/+page.server.ts
+++
@@ -1,7 +0,0 @@
-// src/routes/join/[...bad]/+page.server.ts
-import { error } from '@sveltejs/kit';
-
-/** @type {import('./$types').PageServerLoad} */
-export function load() {
-	throw error(404, 'Invalid lobby code');
-}
