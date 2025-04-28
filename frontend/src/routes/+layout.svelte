86f27d907fd59ffe7a4971978160fbe06b52bf50
Edward Källstedt <edward.kallstedt@gmail.com>
2025-04-28 20:10:31 +0200

    add umami analytics support

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,8 +1,19 @@
 <script lang="ts">
 	import { ModeWatcher } from 'mode-watcher';
 	import '../app.css';
+	import { PUBLIC_UMAMI_WEBSITE_ID } from '$env/static/public';
 	let { children } = $props();
 </script>
+
+<svelte:head>
+	{#if PUBLIC_UMAMI_WEBSITE_ID}
+		<script
+			defer
+			src="https://cloud.umami.is/script.js"
+			data-website-id={PUBLIC_UMAMI_WEBSITE_ID}
+		></script>
+	{/if}
+</svelte:head>
 
 <ModeWatcher />
 {@render children()}
