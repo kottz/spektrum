4098b615098c3adbaf26f9d8bacbfbd5be97ac5f
Edward Källstedt <edward.kallstedt@gmail.com>
2025-11-13 06:47:50 +0100

    frontend: make umami analytics optional

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,17 +1,18 @@
 <script lang="ts">
 	import { ModeWatcher } from 'mode-watcher';
 	import '../app.css';
-	import { PUBLIC_TITLE, PUBLIC_UMAMI_WEBSITE_ID } from '$env/static/public';
+	import { PUBLIC_TITLE } from '$env/static/public';
+	import { env } from '$env/dynamic/public';
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<title>{PUBLIC_TITLE}</title>
-	{#if PUBLIC_UMAMI_WEBSITE_ID}
+	{#if env.PUBLIC_UMAMI_WEBSITE_ID}
 		<script
 			defer
 			src="https://cloud.umami.is/script.js"
-			data-website-id={PUBLIC_UMAMI_WEBSITE_ID}
+			data-website-id={env.PUBLIC_UMAMI_WEBSITE_ID}
 		></script>
 	{/if}
 </svelte:head>
